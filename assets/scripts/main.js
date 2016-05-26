dff00e42efbf4e1c134cf6d168b7521dd122bbb4
Maarten Dings <mdings@x-com.nl>
2016-05-26 12:32:00 +0200

    Changing template rendering order

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,57 +1,44 @@
-(function(window) {
+var current;
 
-  // exit if the browser implements that event
-  if ( "onhashchange" in window.document.body ) { return; }
-
-  var location = window.location,
-    oldURL = location.href,
-    oldHash = location.hash;
-
-  // check the location hash on a 100ms interval
-  setInterval(function() {
-    var newURL = location.href,
-      newHash = location.hash;
-
-    // if the hash has changed and a handler has been bound...
-    if ( newHash != oldHash && typeof window.onhashchange === "function" ) {
-      // execute the handler
-      window.onhashchange({
-        type: "hashchange",
-        oldURL: oldURL,
-        newURL: newURL
-      });
-
-      oldURL = newURL;
-      oldHash = newHash;
-    }
-  }, 100);
-
-})(window)
-
-function request(url){
+function request (url, section = null) {
     var pageRequest = false
 
-    if (!pageRequest && typeof XMLHttpRequest != 'undefined') {
-        pageRequest = new XMLHttpRequest()
-    }
+    if (current != url) {
+        if (!pageRequest && typeof XMLHttpRequest != 'undefined') {
+            pageRequest = new XMLHttpRequest()
+        }
 
-    if (pageRequest) { //if pageRequest is not false
-       pageRequest.open('GET', url, false) //get page synchronously
-       pageRequest.send(null)
-       embed(pageRequest)
+        if (pageRequest) { //if pageRequest is not false
+           pageRequest.open('GET', url, false) //get page synchronously
+           pageRequest.send(null)
+           current = url;
+           embed(pageRequest)
+           // scroll to the element if section is provided
+           jumpTo(section);
+       }
+   } else {
+       jumpTo(section);
    }
 }
 
-function embed(request) {
+function jumpTo (section) {
+    if(document.getElementById(section)) {
+        window.scrollTo(0, document.getElementById(section).offsetTop);
+    }
+}
+
+function embed (request) {
+    console.log('embeddding');
     // if viewing page offline or the document was successfully retrieved online (status code=2000)
     if (window.location.href.indexOf("http")==-1 || request.status==200) {
         document.getElementById('styleguide__main').innerHTML = request.responseText;
         // reload prism
         Prism.highlightAll();
+        bindTogglers();
     }
 }
 
-function bindTogglers() {
+function bindTogglers () {
   var togglers = document.querySelectorAll('.styleguide__icon--code');
   for (var i=0;i<togglers.length;i++) {
     togglers[i].addEventListener('click', function(){
@@ -62,15 +49,18 @@ function bindTogglers() {
   }
 }
 
-var links = document.querySelector('.styleguide__section');
-links.addEventListener('change', function(e){
-    var href = this.value;
-    request(href + '.html');
-    bindTogglers();
-    e.preventDefault();
-});
+var links = document.querySelectorAll('.styleguide__nav a');
+for (var i=0; i<links.length; i++) {
+    links[i].addEventListener('click', function(e){
+        var href = this.getAttribute('href');
+        var url = href.split('#').shift();
+        var section = href.split('#').pop();
+        request(url, section);
+        e.preventDefault();
+    });
+}
+
 
 // load the first option from the dropdown by default
-var href = links[0].value;
-request(href + '.html');
-bindTogglers();
+var href = links[0].getAttribute('href');
+request(href);
--- index.js
+++ index.js
@@ -10,108 +10,106 @@ var marked = require('marked');
 
 /*
 Defines the Styleguide class */
-function Styleguide (options, sections, css) {
-  var defaults = {
-    output: 'styleguide',
-    assets: {},
-    css: css
-  }
-  this.options = utils.extend (defaults, options);
-  this.sections = sections;
-  this.init();
+function Styleguide (options, pages) {
+    var defaults = {
+        output: 'styleguide',
+        assets: {}
+    }
+    this.options = utils.extend (defaults, options);
+    this.pages = pages;
+    this.init();
 }
 
 /*
 Creates a new styleguide instance */
 Styleguide.prototype.init = function () {
-  this.createIndexFile();
-  this.createSections();
+    var pages = this.pages;
+    this.createIndexFile();
+    Object.keys(pages).forEach(function(page) {
+        this.createSections(pages[page], page);
+    }.bind(this));
 }
 
 /*
 Creates the master index template in the output folder */
 Styleguide.prototype.createIndexFile = function () {
-  var template = [__dirname, 'assets/templates/index.pug'].join('/');
-  var data = utils.render (template, {sections: this.sections, options: this.options});
-  utils.write ([this.options.output, 'index.html'].join('/'), data)
+    var template = [__dirname, 'assets/templates/index.pug'].join('/');
+    var data = utils.render (template, {pages: this.pages, options: this.options});
+    utils.write ([this.options.output, 'index.html'].join('/'), data)
 }
 
 /*
 Creates the individual sections */
-Styleguide.prototype.createSections = function () {
-  var sections = this.sections;
-  var options = this.options;
-
-  // loop over each section
-  Object.keys(sections).forEach (function(section) {
+Styleguide.prototype.createSections = function (section, page) {
     var promises = [];
 
     // each section can have multiple demo blocks
     // we need to keep track of their async rendering status before
     // they can be written to file
-    var demos = sections[section].map(function(attr){
-      return attr.attributes.demo;
+    var demos = section.map(function(attr){
+        return attr.attributes.demo;
     });
-
 
     // push the demos in the promises array
     for (var i=0; i<demos.length; i++) {
-      var body = marked (sections[section][i].body);
-      var title = (sections[section][i].attributes.title);
-      promises.push (demo(demos[i], title, body, options));
+        var body = marked(section[i].body);
+        var title = section[i].attributes.title;
+        promises.push(demo(demos[i], title, body, this.options));
     }
 
     // resolve all demo's and only then write to file
     Q.all(promises)
-      .then(function (values) {
+    .then(function(values) {
         var data = {
-          examples: values
+            examples: values
         }
 
         // compile the sections
-        var file = [section, 'html'].join('.');
+        var file = [page, 'html'].join('.');
         var template = [__dirname, 'assets/templates/section.pug'].join('/');
         var result = utils.render(template, data);
 
         // write the file
-        utils.write ([options.dest, file].join('/'), result);
-      });
-  });
+        utils.write ([this.options.dest, file].join('/'), result);
+    }.bind(this));
 }
 
 module.exports = postcss.plugin ('styleguide', function styleguide (options) {
 
-  return function (css, result) {
-    var sections = {};
-    var input = css.source.input.file;
-    options = options || {} ;
+    return function (css, result) {
+        var pages = {};
+        var input = css.source.input.file;
+        options = options || {} ;
 
-    if (options.src && (input.indexOf(options.src) > -1)) {
-      // create the path to the styleguide
-      utils.createPath (options.dest, function () {
-        // write the css to the styleguide
-        utils.write ([options.dest, options.src].join('/'), css.toString());
-        // walk the css
-        css.walkComments (function (comment) {
-          // test if comment is valid front-matter
-          if (fm.test (comment.text)) {
-            var content = fm (comment.text);
-            if (content.attributes.demo) {
-              // create the object key if it doesn't exist yet
-              var key = content.attributes.section.toDash();
-              if (!sections[key]) sections[key] = [];
-              sections[key].page = {
-                name: content.attributes.section,
-                url: key
-              };
-              sections[key].push(content);
-            } else {
-              result.warn ('Missing demo attribute');
-            }
-          }
-        });
-        new Styleguide (options, sections, css);
-      }.bind(this));
+        if (options.src && (input.indexOf(options.src) > -1)) {
+            // create the path to the styleguide
+            utils.createPath (options.dest, function () {
+                // write the css to the styleguide
+                utils.write ([options.dest, options.src].join('/'), css.toString());
+                // walk the css
+                css.walkComments (function (comment) {
+                    // test if comment is valid front-matter
+                    if (fm.test (comment.text)) {
+                        var content = fm (comment.text);
+                        if (content.attributes.demo) {
+                            // create the object key if it doesn't exist yet
+                            var key = content.attributes.section.toDash();
+                            if (!pages[key]) {
+                                pages[key] = [];
+                                pages[key]['sections'] = []
+                            }
+                            /* @TODO: Consolidate the sections into one array */
+                            pages[key].name = content.attributes.section;
+                            pages[key].url = key;
+                            pages[key].push(content);
+                            pages[key]['sections'].push(content);
+                        } else {
+                            result.warn ('Missing demo attribute');
+                        }
+                    }
+                });
+                new Styleguide (options, pages);
+            }.bind(this));
+        }
     }
-  }
 })
